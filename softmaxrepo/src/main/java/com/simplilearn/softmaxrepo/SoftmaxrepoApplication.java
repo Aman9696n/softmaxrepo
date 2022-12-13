3d81b0a02ed24e30b3993956d71671a6deea0704
Aman9696n <amanmishra9696@gmail.com>
2022-12-13 16:18:47 +0000

    this is the spring boot project

---
+++ softmaxrepo/src/main/java/com/simplilearn/softmaxrepo/SoftmaxrepoApplication.java
@@ -0,0 +1,13 @@
+package com.simplilearn.softmaxrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftmaxrepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftmaxrepoApplication.class, args);
+	}
+
+}
---
+++ softmaxrepo/src/test/java/com/simplilearn/softmaxrepo/SoftmaxrepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.softmaxrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftmaxrepoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
